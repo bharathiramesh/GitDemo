0640b91594fae2c29822cf9f1c36c9834f2978ba
bharathi <bharathirameshtcs@gmail.com>
2019-03-10 10:04:49 -0500

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void bangalore()
+	{
+		System.out.println("bangalore");
+		
+	}
+	@Test
+	public void chennai()
+	{
+		System.out.println("chennai");
+	}
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	@Test
+	public void ba()
+	{
+		System.out.println("ba");
+		
+	}
+	@Test
+	public void eu()
+	{
+		System.out.println("eu");
+	}
+
+}
